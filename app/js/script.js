6c59bb0152472345b3151ea0362a0024cdec9aa9
rluna15 <58538880+rluna15@users.noreply.github.com>
2021-11-14 22:38:26 -0500

    working accordion

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,22 @@
 const btn = document.querySelectorAll('.card-accordion-btn');
 const panel = document.querySelectorAll('.card-accordion-panel');
 
+/* document.querySelectorAll('.card-accordion-btn').forEach(btn => {
+	btn.addEventListener('click', event => {
+
+	if(!btn.classList.contains("active-btn")){
+		btn.nextElementSibling.classList = "active";
+	}else {
+		btn.nextElementSibling.classList.remove("active");
+	}
+
+		btn.classList.toggle("active-btn");
+		btn.nextElementSibling.classList.toggle("active");
+	});
+}); */
+
+/* Working have way */
+/*
 for (let i = 0; i < btn.length; i++) {
 	btn[i].addEventListener("click", function () {
 
@@ -13,5 +29,21 @@ for (let i = 0; i < btn.length; i++) {
 				btn[i].classList.toggle("active-btn");
 			}
 		}
+
 	});
+} */
+
+btn.forEach(btn => {
+	btn.addEventListener('click', event => {
+		Hide();
+		btn.classList.add("active-btn");
+		btn.nextElementSibling.classList.add("active");
+	});
+});
+
+function Hide() {
+	for (let i = 0; i < btn.length; i++) {
+		btn[i].classList.remove("active-btn");
+		btn[i].nextElementSibling.classList.remove("active");		
+	}
 }
